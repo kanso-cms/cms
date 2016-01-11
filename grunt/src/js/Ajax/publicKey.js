9f1850040aa8946b1a30f024bf8cb7b4f3f993b6
Joe Howard <joe@pencilscoop.com>
2016-01-11 13:52:13 +1100

    Moved front-end to it's own repo

--- grunt/src/js/Ajax/publicKey.js
+++
@@ -1,7 +0,0 @@
-// ##############################################################################
-// FILE: Ajax/publicKey.js
-// ##############################################################################
-var GLOBAL_PUBLIC_KEY = '';
-var GLOBAL_AJAX_ENABLED = false;
-var GLOBAL_AJAX_URL = window.location.href.replace(/admin(.+)/, 'admin/');
-var GLOBAL_AJAX_QUEUE = [];
