3ee1fb42fdb5b5e562e7b6c058b9fda4d1b061bd
Joe Howard <joey-j@users.noreply.github.com>
2017-05-06 14:36:30 +1000

    Git bugfix

--- src/Kanso/Install.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Kanso application install file
- * This file is deliberately left empty. It only needs to exists
- * for Kanso to be considered "uninstalled", which triggers the install 
- * process. Kanso will delete it after the application first installs.
- */
