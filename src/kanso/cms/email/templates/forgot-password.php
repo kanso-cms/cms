b1175593a2d93064c9a42f43cef80385eb22d9be
Joe J. Howard <joejhoward@mail.com>
2017-10-03 21:40:08 +1100

    Typo in forgot password/username email

--- src/kanso/cms/email/templates/forgot-password.php
+++ src/kanso/cms/email/templates/forgot-password.php
@@ -1,7 +1,7 @@
 <p style="text-align: center;font-size: <?php echo $font_size_h3;?>;font-weight: 300;color: <?php echo $body_color;?>;">Reset Your Password</p>
 <p>Hi <?php echo $name;?>,</p>
 <p>Someone has requested that your password be reset on <a href="<?php echo $websiteUrl;?>" style="color: <?php echo $link_color;?>"><?php echo $websiteName;?></a></p>
-<p>If this wasn't you, please ensure your email secure is as someone may be trying to compromise your account.</p>
+<p>If this wasn't you, please ensure your email is secure as someone may be trying to compromise your account.</p>
 <p>If this was you however, you can reset your Kanso account password by clicking the link below:</p>
 <p><a href="<?php echo $resetUrl;?>" style="color: <?php echo $link_color;?>"><?php echo $resetUrl;?></a></p>
 <table>
