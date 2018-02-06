e5bd8ad8403f2bacf7be4f1238eba078633a7049
Joe J. Howard <joe.howard.au@icloud.com>
2018-02-06 15:45:18 +1100

    Completed OpenSSL Crypto Testing

---
+++ tests/unit/security/crypto/encrypters/OpenSSLTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @copyright Joe J. Howard
+ * @license   https://github.com/kanso-cms/cms/blob/master/LICENSE
+ */
+
+namespace tests\unit\security\crypto\encrypters;
+
+use tests\TestCase;
+use kanso\framework\security\crypto\encrypters\OpenSSL;
+
+/**
+ * @group unit
+ */
+class OpenSSLTest extends TestCase
+{
+	/**
+	 *
+	 */
+	public function testEncryptDecrypt()
+	{
+		$data = 'foobar!!$#$@#"$#@!$P:{';
+
+		$encrypter = new OpenSSL('secret-code', 'AES-256-ECB');
+
+		$hashed = $encrypter->encrypt($data);
+
+		$this->assertEquals($data, $encrypter->decrypt($hashed));
+	}
+
+	/**
+	 *
+	 */
+	public function testCyphers()
+	{
+		$data = 'foobar!!$#$@#"$#@!$P:{';
+
+		foreach (openssl_get_cipher_methods() as $cypher)
+		{
+			$encrypter = new OpenSSL('secret-code', $cypher);
+
+			$hashed = $encrypter->encrypt($data);
+
+			$this->assertEquals($data, $encrypter->decrypt($hashed));
+		}
+	}
+}
