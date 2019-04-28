760082cdae5738e4b6468b9da409f7631cac68b3
Joe J. Howard <joe.howard.au@icloud.com>
2019-04-28 21:32:07 +1000

    Added SQL Builder Trait

---
+++ kanso/framework/common/SqlBuilderTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @copyright Joe J. Howard
+ * @license   https://github.com/kanso-cms/cms/blob/master/LICENSE
+ */
+
+namespace kanso\framework\common;
+
+use kanso\framework\database\query\Builder;
+use kanso\Kanso;
+
+/**
+ * SQL Builder Trait.
+ *
+ * @author Joe J. Howard
+ */
+trait SqlBuilderTrait
+{
+	/**
+     * SQL query builder instance
+     *
+     * @var \kanso\framework\database\query\Builder|null
+     */
+    protected $sql = null;
+
+    /**
+     * Instantiate and/or return a query builder instance
+     *
+     * @access protected
+     * @return \kanso\framework\database\query\Builder
+     */
+    protected function sql(): Builder
+    {
+        if (is_null($this->sql))
+        {
+            $this->sql = Kanso::instance()->Database->connection()->builder();
+        }
+
+        return $this->sql;
+    }
+}
